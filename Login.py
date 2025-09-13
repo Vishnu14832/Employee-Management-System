53163c167abd16236c1258ef0ca44ec523307e1a
Vishnu Srivastava <vishnusrivastava442@gmail.com>
2025-09-13 22:54:52 +0530

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,37 @@
+from customtkinter import *
+from PIL import Image
+from tkinter import messagebox
+
+def login():
+    if usernameEntry.get() == "" or passwordEntry.get() == "":
+        messagebox.showerror("Error","Fill all the details")
+    elif usernameEntry.get() == "Vishnu" and passwordEntry.get() == "1234":
+        messagebox.showinfo("Success","Login Successful")
+        root.destroy()
+        import ems
+    else:
+        messagebox.showerror("Error","Wrong credentials")
+
+root = CTk()
+root.geometry("930x478")
+root.resizable(0,0)
+root.title("Login Page")
+root.configure(fg_color="#161C30")
+
+image = CTkImage(Image.open("bg.jpg"),size=(474,500))
+imageLabel = CTkLabel(root,image=image,text="")
+imageLabel.place(x=500,y=0)
+
+headingLabel = CTkLabel(root, text='Employee Management System',text_color = "dark blue",
+font=("Goudy Old Style",30,"bold"))
+headingLabel.place(x=30,y=100)
+
+usernameEntry = CTkEntry(root,placeholder_text="Enter Username",text_color = "white",width=200)
+usernameEntry.place(x=100,y=160)
+
+passwordEntry = CTkEntry(root,placeholder_text="Enter Password",text_color = "white",show="*",width=200)
+passwordEntry.place(x=100,y=200)
+
+loginButton = CTkButton(root,text="Login",cursor="hand2",corner_radius=10,command=login)
+loginButton.place(x=130,y=250)
+root.mainloop()
---
+++ ems.py
@@ -0,0 +1,59 @@
+from customtkinter import *
+from PIL import Image
+
+window = CTk()
+window.title("Employee Management System")
+window.geometry("930x600")
+window.resizable(False,False)
+window.configure(fg_color="#161C30")
+
+logo=CTkImage(Image.open("header.jpg"),size=(930,158))
+logoLabel = CTkLabel(window,image=logo,text="")
+logoLabel.grid(row=0,column=0,columnspan=2)
+
+leftFrame = CTkFrame(window,fg_color="#161C30")
+leftFrame.grid(row=1,column=0)
+
+idLabel = CTkLabel(leftFrame,text="Id",font=("Arial",18,"bold"))
+idLabel.grid(row=0,column=0,padx=20,pady=15,sticky='w')
+
+idEntry = CTkEntry(leftFrame,font=("Arial",15,"bold"),width=180)
+idEntry.grid(row=0,column=1)
+
+nameLabel = CTkLabel(leftFrame,text="Name",font=("Arial",18,"bold"))
+nameLabel.grid(row=1,column=0,padx=20,pady=15,sticky='w')
+
+nameEntry = CTkEntry(leftFrame,font=("Arial",15,"bold"),width=180)
+nameEntry.grid(row=1,column=1)
+
+phoneLabel = CTkLabel(leftFrame,text="Contact",font=("Arial",18,"bold"))
+phoneLabel.grid(row=2,column=0,padx=20,pady=15,sticky='w')
+
+phoneEntry = CTkEntry(leftFrame,font=("Arial",15,"bold"),width=180)
+phoneEntry.grid(row=2,column=1)
+
+roleLabel = CTkLabel(leftFrame,text="Role",font=("Arial",18,"bold"))
+roleLabel.grid(row=3,column=0,padx=20,pady=15,sticky='w')
+
+role_options=['Web Developer','Data Scientist','Ai Engineer','Network Engineer','IT Consultant','UI UX Designer','Cloud Architect','Technical Writer']
+
+roleBox = CTkComboBox(leftFrame,values=role_options,width=180,font=("Arial",18,"bold"))
+roleBox.grid(row=3,column=1)
+
+genderLabel = CTkLabel(leftFrame,text="Gender",font=("Arial",18,"bold"))
+genderLabel.grid(row=4,column=0,padx=20,pady=15,sticky='w')
+
+gender_options=['Male','Female']
+genderBox = CTkComboBox(leftFrame,values=gender_options,width=180,font=("Arial",18,"bold"))
+genderBox.grid(row=4,column=1)
+
+salaryLabel = CTkLabel(leftFrame,text="Salary",font=("Arial",18,"bold"))
+salaryLabel.grid(row=5,column=0,padx=20,pady=15,sticky='w')
+
+salaryEntry = CTkEntry(leftFrame,font=("Arial",15,"bold"),width=180)
+salaryEntry.grid(row=5,column=1)
+
+
+rightFrame = CTkFrame(window)
+rightFrame.grid(row=1,column=1)
+window.mainloop()
