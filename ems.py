15b256a8e0f414f740cd95ee8fec86240b138714
Vishnu Srivastava <vishnusrivastava442@gmail.com>
2025-09-17 23:14:35 +0530

    ems.py

--- ems.py
+++ ems.py
@@ -1,9 +1,52 @@
 from customtkinter import *
 from PIL import Image
+from tkinter import ttk,messagebox
+import database
+
+def clear():
+    idEntry.delete(0, END)
+    nameEntry.delete(0, END)
+    phoneEntry.delete(0, END)
+    roleBox.set('web developer')
+    genderBox.set('Male')
+    salaryEntry.delete(0, END)
+
+def selection(event):
+    selected_item = tree.selection()
+    if selected_item:
+        row = tree.item(selected_item)['values']
+        clear()
+        idEntry.insert(0, row[0])
+        nameEntry.insert(0, row[1])
+        phoneEntry.insert(0, row[2])
+        roleBox.set(row[3])
+        genderBox.set(row[4])
+        salaryEntry.insert(0,row[5])
+
+
+def treeview_data():
+    employees = database.fetch_employee()
+    tree.delete(*tree.get_children())
+
+    for employee in employees:
+        tree.insert('',END,values=employee)
+
+def add_employee():
+    if nameEntry.get() == "" or phoneEntry.get() == "" or roleBox.get() == "" or genderBox.get() == "" or salaryEntry.get() == "" or idEntry.get() == "":
+        messagebox.showerror("Error", "Please fill all fields")
+
+    elif database.id_exists(idEntry.get()):
+        messagebox.showerror("Error", "id already exists")
+
+    else:
+        database.insert(idEntry.get(),nameEntry.get(),phoneEntry.get(),roleBox.get(),genderBox.get(),salaryEntry.get())
+        treeview_data()
+        clear()
+        messagebox.showinfo("Success","Data Added Successfully")
 
 window = CTk()
 window.title("Employee Management System")
-window.geometry("930x600")
+window.geometry("930x600+100+100")
 window.resizable(False,False)
 window.configure(fg_color="#161C30")
 
@@ -53,7 +96,69 @@ salaryLabel.grid(row=5,column=0,padx=20,pady=15,sticky='w')
 salaryEntry = CTkEntry(leftFrame,font=("Arial",15,"bold"),width=180)
 salaryEntry.grid(row=5,column=1)
 
-
 rightFrame = CTkFrame(window)
 rightFrame.grid(row=1,column=1)
+
+search_options = ['Id','Name','Contact','Role','Gender','Salary']
+searchBox = CTkComboBox(rightFrame,values=search_options,state='readonly')
+searchBox.set('Search By')
+searchBox.grid(row=0,column=0)
+
+searchEntry = CTkEntry(rightFrame)
+searchEntry.grid(row=0,column=1)
+
+searchButton = CTkButton(rightFrame,text='Search',width=100)
+searchButton.grid(row=0,column=2)
+
+showallButton = CTkButton(rightFrame,text='Show All',width=100)
+showallButton.grid(row=0,column=3,pady=5)
+
+tree = ttk.Treeview(rightFrame,height=13)
+tree.grid(row=1,column=0,columnspan=4)
+
+tree['columns'] = ('Id','Name','Contact','Role','Gender','Salary')
+tree.heading('Id',text='Id')
+tree.heading('Name',text='Name')
+tree.heading('Contact',text='Contact')
+tree.heading('Role',text='Role')
+tree.heading('Gender',text='Gender')
+tree.heading('Salary',text='Salary')
+
+tree.config(show='headings')
+
+tree.column('Id',width=100)
+tree.column('Name',width=160)
+tree.column('Contact',width=160)
+tree.column('Role',width=200)
+tree.column('Gender',width=100)
+tree.column('Salary',width=140)
+
+style = ttk.Style()
+style.configure('Treeview.Heading',font=("Arial",18,"bold"))
+style.configure('Treeview',font=("Arial",18,"bold"),rowheight=30,background="#161C30",foreground="white")
+
+scrollbar = ttk.Scrollbar(rightFrame,orient=VERTICAL)
+scrollbar.grid(row=1,column=4,sticky='ns')
+
+buttonFrame = CTkFrame(window,fg_color="#161C30")
+buttonFrame.grid(row=2,column=0,columnspan=2)
+
+newButton = CTkButton(buttonFrame,text="New Employee",font=("Arial",15,"bold"),width=160,corner_radius=15)
+newButton.grid(row=0,column=0,pady=5,padx=5)
+
+updateButton = CTkButton(buttonFrame,text="Update Employee",font=("Arial",15,"bold"),width=160,corner_radius=15)
+updateButton.grid(row=0,column=1,pady=5,padx=5)
+
+deleteButton = CTkButton(buttonFrame,text="Delete Employee",font=("Arial",15,"bold"),width=160,corner_radius=15)
+deleteButton.grid(row=0,column=2,pady=5,padx=5)
+
+addButton = CTkButton(buttonFrame,text="Add Employee",font=("Arial",15,"bold"),width=160,corner_radius=15,command=add_employee)
+addButton.grid(row=0,column=3,pady=5,padx=5)
+
+deleteallButton = CTkButton(buttonFrame,text="Delete All",font=("Arial",15,"bold"),width=160,corner_radius=15)
+deleteallButton.grid(row=0,column=4,pady=5,padx=5)
+
+treeview_data()
+window.bind('<ButtonRelease>',selection)
+
 window.mainloop()
